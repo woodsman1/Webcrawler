afd482ff6b83fcc34f7145b094c7e85c1bb6765b
woodsman1 <19ucc114@lnmiit.ac.in>
2022-01-26 00:14:33 +0530

    working prototype 0.0

--- crawler/trial.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse as urlparse
-
-parsed = urlparse.urlparse('../dfahsdjkfa/path;parameters?query=argument#fragment')
-# parsed = parsed._replace(netloc = 'Mohit.com', scheme = 'http')
-print (parsed.geturl())
-print (parsed.path)
